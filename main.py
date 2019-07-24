902b68b5012d651a001e5c44b642a3179e9ac000
e_dragon <info@electrodragon.com>
2019-07-25 00:43:06 +0800

    json

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-#input 
-#print 
+#input and save
+#update and print 
 #API update current price
  
 import json
 
 file = open('data.json', 'w+')
-data = { "name": 'DBC', "share": 1, "price": 21.43 }
+#data = { "name": 'DBC', "share": 1, "price": 21.43 }
+name = raw_input("Stock name? ")
+share = raw_input("Share? ")
+price = raw_input("price? ")
 
-json.dump(data, file)
+new_data = {
+	name: [
+	{'share':share, 'price': price}
+]
+}
+
+json.dump(new_data, file)
